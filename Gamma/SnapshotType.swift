62c1e77648998418ba58fad1d37e4420444a09fa
Dalton Claybrook <daltonclaybrook@gmail.com>
2018-01-05 00:39:44 -0600

    Added function for getting device model name

--- Gamma/SnapshotType.swift
+++ Gamma/SnapshotType.swift
@@ -22,12 +22,28 @@ extension SnapshotType {
             return .error(SnapshotError.missingEnvironmentVariable)
         }
         
+        let model = deviceModel()
         return .match
     }
     
     // MARK: Private
     
-    func dataFilePath() -> String? {
+    private func deviceModel() -> String? {
+        if let simulatorModel = ProcessInfo.processInfo.environment["SIMULATOR_MODEL_IDENTIFIER"] {
+            return simulatorModel
+        }
+        
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        return identifier
+    }
+    
+    private func dataFilePath() -> String? {
         return ProcessInfo.processInfo.environment["GAMMA_DIR"]
     }
 }
