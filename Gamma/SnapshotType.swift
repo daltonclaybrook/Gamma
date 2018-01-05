dbf4b49e3ebdcffa69e83f62eba20c3aec8ee25c
Dalton Claybrook <daltonclaybrook@gmail.com>
2018-01-05 00:46:01 -0600

    Tweaking the device model logic

--- Gamma/SnapshotType.swift
+++ Gamma/SnapshotType.swift
@@ -33,14 +33,11 @@ extension SnapshotType {
             return simulatorModel
         }
         
-        var systemInfo = utsname()
-        uname(&systemInfo)
-        let machineMirror = Mirror(reflecting: systemInfo.machine)
-        let identifier = machineMirror.children.reduce("") { identifier, element in
-            guard let value = element.value as? Int8, value != 0 else { return identifier }
-            return identifier + String(UnicodeScalar(UInt8(value)))
-        }
-        return identifier
+        var size: size_t = 0
+        sysctlbyname("hw.machine", nil, &size, nil, 0)
+        var machine = [CChar](repeating: 0, count: Int(size))
+        sysctlbyname("hw.machine", &machine, &size, nil, 0)
+        return String(cString: machine)
     }
     
     private func dataFilePath() -> String? {
